387f84fa8045babc4b800b97354869876eb6a7a0
Larry Hernandez <larryhernanjr@gmail.com>
2017-01-14 15:38:04 -0500

    fix naming conventions. add base, index views

--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'miguel'}
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'beautiful day in portland!'
+        },
+        {
+            'author': {'nickname': 'susano'},
+            'body': 'The Avengers was great...'
+        }
+    ]
     return render_template('index.html',
                             title='Home',
-                            user=user)
+                            user=user,
+                            posts=posts)
