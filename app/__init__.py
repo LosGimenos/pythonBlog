57348cb9c757d2a5c9aa97a8bfc5242f84d877b6
Larry Hernandez <larryhernanjr@gmail.com>
2017-01-26 16:45:43 -0500

    add logic to negate duplicate nicknames. add field errors to user form view

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_openid import OpenID
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@')
++ MAIL_SERVER, ADMINS, 'micoblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.longer.addHandler(mail_handler)
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 102, 4, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 app = Flask(__name__)
 app.config.from_object('config')
